8d4627a8d1c1e5997746be03e54908da2e15223e
i-yard <iyardsev@gmail.com>
2019-04-08 18:13:32 +0300

    first step

--- src/Cli.php
+++ src/Cli.php
@@ -1,10 +1,25 @@
 <?php
 namespace Gendiff\Cli;
 
+use \Docopt;
 use function \cli\line;
 use function \cli\prompt;
 
+const DOC = <<<DOC
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
+
+Options:
+  -h --help     	Show this screen
+  --format <fmt>	Report format [default: pretty]
+  --version     	Show version
+
+DOC;
+
 function run()
 {
-    line('Welcome to the gendiff!');
+    $args = Docopt::handle(DOC);
 }
