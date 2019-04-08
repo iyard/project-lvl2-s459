e1080f31f18eb44f2c2facf906a884b8d06e14e2
i-yard <iyardsev@gmail.com>
2019-04-08 15:29:19 +0300

    Start project

---
+++ README.md
@@ -0,0 +1,2 @@
+# About
+gendiff - search for differences in the configuration files
---
+++ src/Cli.php
@@ -0,0 +1,10 @@
+<?php
+namespace Gendiff\Cli;
+
+use function \cli\line;
+use function \cli\prompt;
+
+function run()
+{
+    line('Welcome to the gendiff!');
+}
